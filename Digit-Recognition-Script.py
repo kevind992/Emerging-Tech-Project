7410e6bb4252659095d35af96dfb60f66a8dce0e
Kevin Delassus <g00270791@gmit.ie>
2018-10-10 20:18:00 +0100

    Four Jupyter notebooks and one Python Script file added to repository

---
+++ Digit-Recognition-Script.py
@@ -0,0 +1,4 @@
+# Author: Kevin Delassus - G00270791
+# A python script that takes an image file containing
+# handwitten digit and identifies the digit using a super-
+# vised learing algorithm and the MNIST dataset.
