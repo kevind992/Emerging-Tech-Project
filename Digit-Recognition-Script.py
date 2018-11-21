b7981526c57e105ef64e28569affc976d637287e
Kevin <kevind992@hotmail.com>
2018-11-21 21:34:22 +0000

    Finished introduction to mnist dataset

--- Digit-Recognition-Script.py
+++ Digit-Recognition-Script.py
@@ -2,3 +2,51 @@
 # A python script that takes an image file containing
 # handwitten digit and identifies the digit using a super-
 # vised learing algorithm and the MNIST dataset.
+
+# Adapted from: https://docs.python.org/3/library/gzip.html
+
+import gzip
+import numpy as np
+from PIL import Image
+
+with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+    file_content = f.read()
+type(file_content)
+file_content[0:4]
+
+# Training the Nural Network
+# Import keras.
+import keras as kr
+
+# Start a neural network, building it by layers.
+model = kr.models.Sequential()
+
+# Add a hidden layer with 1000 neurons and an input layer with 784.
+model.add(kr.layers.Dense(units=1000, activation='relu', input_dim=784))
+# Add a three neuron output layer.
+model.add(kr.layers.Dense(units=10, activation='softmax'))
+
+# Build the graph.
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+    train_img = f.read()
+
+with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+    train_lbl = f.read()
+    
+train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8)
+train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
+
+inputs = train_img.reshape(60000, 784)
+
+# For encoding categorical variables.
+import sklearn.preprocessing as pre
+
+encoder = pre.LabelBinarizer()
+encoder.fit(train_lbl)
+outputs = encoder.transform(train_lbl)
+
+outputs[0]
+
+model.fit(inputs, outputs, epochs=15, batch_size=10)
